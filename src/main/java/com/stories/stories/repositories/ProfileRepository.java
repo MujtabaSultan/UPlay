d2d877770597138a7528c607923c673a38da489a
Your MujtabaSultan <b.mojtabaalilol@gmail.com>
2026-05-13 15:38:00 +0300

    profile repository

---
+++ src/main/java/com/stories/stories/repositories/ProfileRepository.java
@@ -0,0 +1,9 @@
+package com.stories.stories.repositories;
+
+import com.stories.stories.models.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile,Long> {
+}
