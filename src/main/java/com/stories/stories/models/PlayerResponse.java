b649cfef09bc6bef10a4aedec510b572fa9f8015
Your MujtabaSultan <b.mojtabaalilol@gmail.com>
2026-05-13 09:10:00 +0300

    player response

---
+++ src/main/java/com/stories/stories/models/PlayerResponse.java
@@ -0,0 +1,17 @@
+package com.stories.stories.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PlayerResponse {
+    private Long userId;
+    private String username;
+}
